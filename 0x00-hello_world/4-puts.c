d4b84ee0aff022cb998af24a85293ddded4f838b
Pulexphasha <puliephasha@gmail.com>
2023-02-17 02:30:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <studio.h>
 
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline.
+ * main - prints "Programming is like building a multilingual puzzle, followed by a newline."
  * Return: Always 0.
  */
 
