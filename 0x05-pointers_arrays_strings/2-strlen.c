40a0621408886ce0d70e1b091b02fef6cb7848cf
Pulexphasha <puliephasha@gmail.com>
2023-02-27 20:36:32 +0200

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,13 +6,12 @@
  * Return: The length of @str.
  */
 
-size_t _strlen(const char *str)
+int _strlen(char *s)
 
 {
-	size_t length = 0;
+	int index;
 
-	while (*str++)
-	length++;
-	return (length);
-
+	for (index = 0; s[index] != '\0' ; index++)
+		;
+	return (index);
 }
