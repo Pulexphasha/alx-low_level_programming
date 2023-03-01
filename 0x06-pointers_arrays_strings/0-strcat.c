80fbb353100278fb580329152169b62b82497bd7
Pulexphasha <puliephasha@gmail.com>
2023-03-01 08:29:39 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,13 @@
 char *_strcat(char *dest, char *src)
 
 {
-
 	int dlen = 0, i;
 
 	while (dest[dlen])
 	{
 		dlen++;
 	}
+
 
 	for (i = 0; src[i] != 0; i++)
 	{
@@ -27,6 +27,5 @@ char *_strcat(char *dest, char *src)
 
 	dest[dlen] = '\0';
 	return (dest);
-
 }
 
