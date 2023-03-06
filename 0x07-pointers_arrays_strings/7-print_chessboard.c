6cbb9f98178d60106035ef8f7dea5ea83b24826c
Pulexphasha <puliephasha@gmail.com>
2023-03-06 12:09:03 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,8 +11,8 @@ void print_chessboard(char (*a)[8])
 
 	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[i][n]);
-		_putchar('\n');
+	for (n = 0; n < 8; n++)
+	_putchar(a[i][n]);
+	_putchar('\n');
 	}
 }
