be15c0bc210e0f44fc57fa0418c5f505e02eff7e
Pulexphasha <puliephasha@gmail.com>
2023-03-30 13:11:50 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
 /**
  * print_list - prints all the elements of a list
  * @h: pointer to the list_t list to print
+ *
  * Return: the number of nodes 
  */
 
@@ -18,8 +19,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
+		n++;
 	}
 
-	return (nodes);
+	return (n);
 }
